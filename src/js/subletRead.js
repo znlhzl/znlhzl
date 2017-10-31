200e4109dd5486c2d4383b3ccd1daeef06a6cd42
znlhzl <33115374+znlhzl@users.noreply.github.com>
2017-10-31 09:10:10 +0800

    3
    4

---
+++ src/js/subletRead.js
@@ -0,0 +1,58 @@
+left();
+	
+	
+	/*----------单选--------------*/
+	var radios =document.getElementsByClassName("radios")[0];
+	var year =document.getElementsByClassName("year")[0];
+	var month =document.getElementsByClassName("month")[0];
+	
+	year.onclick=function(){
+		radios.style.display="block";
+		radios.style.right="2.09rem";
+		radios.style.top="0.14rem";
+	}
+	
+	month.onclick=function(){
+		radios.style.display="block";
+		radios.style.right="0.56rem";
+		radios.style.top="0.14rem";
+	}
+	
+	if(year.checked){
+		radios.style.display="block";
+		radios.style.right="2.09rem";
+		radios.style.top="0.14rem";
+	}
+	if(month.checked){
+		radios.style.display="block";
+		radios.style.right="0.56rem";
+		radios.style.top="0.14rem";
+	}
+	
+	var agree= document.getElementsByClassName("agree")[0];
+	
+	var disagree =document.getElementsByClassName("disagree")[0];
+	
+	agree.onclick=function(){
+		agree.style.background="#0082D9";
+		disagree.style.background="none";
+		agree.style.color="#FFFFFF";
+		disagree.style.color="#979797";
+	}
+	disagree.onclick =function(){
+		disagree.style.background="#0082D9";
+		agree.style.background="none";
+		disagree.style.color="#FFFFFF";
+		agree.style.color="#979797";
+	}
+	
+	var ok=document.getElementsByClassName("ok")[0];
+	ok.onclick=function(){
+		if(agree.style.color=="rgb(255, 255, 255)"||disagree.style.color=="rgb(255, 255, 255)"){
+			window.location.href="sublet.html";
+			alert("已审批，点击跳转");
+		}else{
+			alert("请填写审批结果后再确定");
+		}
+		
+	}
