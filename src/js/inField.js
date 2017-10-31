d603dd6980c2f9f95076d24054f88344bd3e4586
znlhzl <33115374+znlhzl@users.noreply.github.com>
2017-10-31 11:35:27 +0800

    3
    3

--- src/js/inField.js
+++ src/js/inField.js
@@ -29,7 +29,7 @@
 				}else if(tests[j].innerHTML=="验机异常"){
 					tests[j].style.color="#FE3F3B";
 					tests[j].style.background="#FAFAFA";
-					tests[j].style.background="green";
+					
 					
 				}
 				
