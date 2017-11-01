8d22905304967718a9ca393904ac7915ff4c05b4
znlhzl <33115374+znlhzl@users.noreply.github.com>
2017-11-01 11:53:37 +0800

    3
    3

--- src/js/servesAllot.js
+++ src/js/servesAllot.js
@@ -13,9 +13,24 @@
     	
         $('#more').append('<div class="engineer1"><span>协作工程师</span><s>请选择协作工程师</s><img src="../img/next.png" class="next"/></div>')
 		we();
+		checknum();
     });
-    
+    /*  ----------控制工程师数量------------------*/
 
+    var helpgcs =document.getElementsByClassName("engineer1s")[0].getElementsByTagName("li");
+    console.log(helpgcs.length);
+    function checknum(){
+    	var numa= document.getElementsByClassName("engineer1");
+    	console.log(numa.length);
+    	if(numa.length==helpgcs.length-1){
+    		var addengineer=document.getElementsByClassName("add-engineer")[0];
+    		addengineer.style.display="none";
+    	}
+    }
+    checknum();
+	
+	
+	
 	function we(){
 		var pins = document.getElementById("more").getElementsByClassName("engineer1");
 		
